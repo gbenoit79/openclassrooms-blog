7effce71d0ae37e5f2706b76949a6224261ccec7
Guillaume BENOIT <gbenoit79@gmail.com>
2018-06-04 17:10:00 +0200

    Update db dump

--- _db/blog.sql
+++ _db/blog.sql
@@ -1,70 +1,101 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Serveur: localhost
--- Généré le : Ven 26 Mars 2010 à 13:57
--- Version du serveur: 5.1.36
--- Version de PHP: 5.3.0
+-- Host: localhost
+-- Generation Time: Jun 04, 2018 at 03:08 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 --
--- Base de données: `test`
+-- Database: `blog`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `billets`
+-- Table structure for table `comments`
 --
 
-DROP TABLE IF EXISTS `billets`;
-CREATE TABLE IF NOT EXISTS `billets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titre` varchar(255) NOT NULL,
-  `contenu` text NOT NULL,
-  `date_creation` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `creation_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `billets`
+-- Dumping data for table `comments`
 --
 
-INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_creation`) VALUES
-(1, 'Bienvenue sur mon blog !', 'Je vous souhaite à toutes et à tous la bienvenue sur mon blog qui parlera de... PHP bien sûr !', '2010-03-25 16:28:41'),
-(2, 'Le PHP à la conquête du monde !', 'C''est officiel, l''éléPHPant a annoncé à la radio hier soir "J''ai l''intention de conquérir le monde !".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu''il n''en fallait pour dire "éléPHPant". Pas dur, ceci dit entre nous...', '2010-03-27 18:31:11');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `commentaires`
---
-
-DROP TABLE IF EXISTS `commentaires`;
-CREATE TABLE IF NOT EXISTS `commentaires` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_billet` int(11) NOT NULL,
-  `auteur` varchar(255) NOT NULL,
-  `commentaire` text NOT NULL,
-  `date_commentaire` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
-
---
--- Contenu de la table `commentaires`
---
-
-INSERT INTO `commentaires` (`id`, `id_billet`, `auteur`, `commentaire`, `date_commentaire`) VALUES
+INSERT INTO `comments` (`id`, `post_id`, `author`, `content`, `creation_date`) VALUES
 (1, 1, 'M@teo21', 'Un peu court ce billet !', '2010-03-25 16:49:53'),
 (2, 1, 'Maxime', 'Oui, ça commence pas très fort ce blog...', '2010-03-25 16:57:16'),
 (3, 1, 'MultiKiller', '+1 !', '2010-03-25 17:12:52'),
-(4, 2, 'John', 'Preum''s !', '2010-03-27 18:59:49'),
-(5, 2, 'Maxime', 'Excellente analyse de la situation !\r\nIl y arrivera plus tôt qu''on ne le pense !', '2010-03-27 22:02:13');
+(4, 2, 'John', 'Preum\'s !', '2010-03-27 18:59:49'),
+(5, 2, 'Maxime', 'Excellente analyse de la situation !\r\nIl y arrivera plus tôt qu\'on ne le pense !', '2010-03-27 22:02:13'),
+(6, 2, 'Guillaume', 'Ceci est un test !', '2018-06-01 21:10:09'),
+(7, 2, 'Robert', 'Ceci est un autre test !', '2018-06-01 21:10:49'),
+(8, 1, 'Pépito', 'Ceci est un super test !', '2018-06-01 21:18:13'),
+(9, 2, 'Pépito', 'C\'est top !', '2018-06-04 15:57:09'),
+(10, 2, 'Alexandre', 'C\'est super', '2018-06-04 16:13:39'),
+(11, 1, 'Pédro', 'C\'est super', '2018-06-04 17:01:29'),
+(12, 1, 'Pépito', 'Youhou', '2018-06-04 17:02:26'),
+(13, 2, 'Pépito', 'C\'est génial', '2018-06-04 17:06:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `creation_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `content`, `creation_date`) VALUES
+(1, 'Bienvenue sur mon blog !', 'Je vous souhaite à toutes et à tous la bienvenue sur mon blog qui parlera de... PHP bien sûr !', '2010-03-25 16:28:41'),
+(2, 'Le PHP à la conquête du monde !', 'C\'est officiel, l\'éléPHPant a annoncé à la radio hier soir \"J\'ai l\'intention de conquérir le monde !\".\r\nIl a en outre précisé que le monde serait à sa botte en moins de temps qu\'il n\'en fallait pour dire \"éléPHPant\". Pas dur, ceci dit entre nous...', '2010-03-27 18:31:11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
