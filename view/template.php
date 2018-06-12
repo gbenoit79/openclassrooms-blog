bc6374c8426e9f361d4bea7ca730aa81766fcbce
Guillaume BENOIT <gbenoit79@gmail.com>
2018-06-12 17:49:49 +0200

    Use Bootstrap

--- view/template.php
+++ view/template.php
@@ -2,8 +2,15 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css" />
+        <!-- Website CSS -->
+        <link href="public/css/style.css" rel="stylesheet" />
+
         <title><?php echo $viewData['title']; ?></title>
-        <link href="public/css/style.css" rel="stylesheet" /> 
     </head>
         
     <body>
